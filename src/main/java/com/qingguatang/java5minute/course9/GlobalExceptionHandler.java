9e6941dfbbf313bf1fea25df072e5b43656c5790
unknown <970608869@qq.com>
2018-06-23 13:55:28 +0800

    init

---
+++ src/main/java/com/qingguatang/java5minute/course9/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.qingguatang.java5minute.course9;
+
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Controller
+public class GlobalExceptionHandler implements ErrorViewResolver {
+
+    @Override
+    public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> model){
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("url",request.getRequestURI());
+        mav.addAllObjects(model);
+        mav.setViewName("error");
+        return mav;
+    }
+}
